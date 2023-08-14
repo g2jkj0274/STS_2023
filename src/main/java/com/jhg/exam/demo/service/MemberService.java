bd4a7a66c9097931e41aba939533d96e4b445c6a
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-14 12:20:51 +0900

    MemberRepository추가 후 회원가입 처리

--- src/main/java/com/jhg/exam/demo/service/MemberService.java
+++ src/main/java/com/jhg/exam/demo/service/MemberService.java
@@ -2,12 +2,17 @@ package com.jhg.exam.demo.service;
 
 import org.springframework.stereotype.Service;
 
+import com.jhg.exam.demo.repository.MemberRepository;
+
 @Service
 public class MemberService {
-	public void join(String loginId, String loginPw, 
-					 String name, String nickName, 
-					 String cellphoneNo, String email) {
-		// TODO Auto-generated method stub
-		
+	private MemberRepository memberRepository;
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
+	public void join(String loginId, String loginPw, String name, String nickname, String cellphoneNo, String email) {
+		memberRepository.join(loginId, loginPw, name, nickname, cellphoneNo, email);
 	}
 }
