9a903bb2379633c2dbcbd6f6cc4991041f4afedd
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-14 10:44:29 +0900

    UsrMemberController, MemberService추가

---
+++ src/main/java/com/jhg/exam/demo/service/MemberService.java
@@ -0,0 +1,13 @@
+package com.jhg.exam.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+	public void join(String loginId, String loginPw, 
+					 String name, String nickName, 
+					 String cellphoneNo, String email) {
+		// TODO Auto-generated method stub
+		
+	}
+}
