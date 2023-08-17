37a20c5ae5522eae51fbd96a882bc223a0c65afb
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-17 10:04:00 +0900

    usr/article/doAdd에 ResultData 적용

--- src/main/java/com/jhg/exam/demo/service/ArticleService.java
+++ src/main/java/com/jhg/exam/demo/service/ArticleService.java
@@ -5,20 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.jhg.exam.demo.repository.ArticleRepository;
+import com.jhg.exam.demo.util.Ut;
 import com.jhg.exam.demo.vo.Article;
+import com.jhg.exam.demo.vo.ResultData;
 
 @Service
 public class ArticleService {
 	ArticleRepository articleRepository;
-	
+
 	// Autowried써도 되고 아래 생성자처럼 해도 된다.
 	public ArticleService(ArticleRepository articleRepository) {
 		this.articleRepository = articleRepository;
 	}
 
-	public int writeArticle(String title, String body) {
+	public ResultData writeArticle(String title, String body) {
 		articleRepository.writeArticle(title, body);
-		return articleRepository.getLastInsertId();
+		int id = articleRepository.getLastInsertId();
+
+		return ResultData.from("S-1", Ut.f("%d번 게시물이 생성되었습니다.", id), id);
 	}
 
 	public List<Article> getArticles() {
