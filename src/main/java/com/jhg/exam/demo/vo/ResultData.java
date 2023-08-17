f16a513bbde0c44cdc9314fd08c2cdd430736649
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-17 10:14:51 +0900

    ResultData 클래스에 제너릭 기능 추가

--- src/main/java/com/jhg/exam/demo/vo/ResultData.java
+++ src/main/java/com/jhg/exam/demo/vo/ResultData.java
@@ -2,40 +2,40 @@ package com.jhg.exam.demo.vo;
 
 import lombok.Getter;
 
-public class ResultData {
+public class ResultData<DT> {
 	@Getter
 	private String resultCode;
 	@Getter
 	private String msg;
 	@Getter
-	private Object data1;
-	
+	private DT data1;
+
 	private ResultData() {
-		
+
 	}
-	
-	public static ResultData from(String reslutCode, String msg, Object data1) {
+
+	public static <DT> ResultData<DT> from(String reslutCode, String msg, DT data1) {
 		ResultData rd = new ResultData();
 		rd.resultCode = reslutCode;
 		rd.msg = msg;
 		rd.data1 = data1;
-		
-		return rd;				
+
+		return rd;
 	}
-	
+
 	public static ResultData from(String resultCode, String msg) {
 		return from(resultCode, msg, null);
 	}
-	
+
 	public boolean isSuccess() {
 		return resultCode.startsWith("S-");
 	}
-	
+
 	public boolean isFail() {
 		return isSuccess() == false;
 	}
-	
-	public static ResultData newData(ResultData joinRd, Object newData) {
-		return from(joinRd.getResultCode(), joinRd.getMsg(), newData);
+
+	public static <DT> ResultData<DT> newData(ResultData oldRd, DT newData) {
+		return from(oldRd.getResultCode(), oldRd.getMsg(), newData);
 	}
 }
