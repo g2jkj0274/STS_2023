91fbfb364be3298675d719116fdbc9e08ec3659f
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-17 09:47:45 +0900

    usr/article/getArticle에 ResultData 적용

--- src/main/java/com/jhg/exam/demo/controller/UsrArticleController.java
+++ src/main/java/com/jhg/exam/demo/controller/UsrArticleController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jhg.exam.demo.service.ArticleService;
+import com.jhg.exam.demo.util.Ut;
 import com.jhg.exam.demo.vo.Article;
+import com.jhg.exam.demo.vo.ResultData;
 
 @Controller
 public class UsrArticleController {
@@ -23,7 +25,7 @@ public class UsrArticleController {
 	@ResponseBody
 	public Article doAdd(String title, String body) {
 		int id = articleService.writeArticle(title, body);
-	
+
 		Article article = articleService.getArticle(id);
 
 		return article;
@@ -38,13 +40,13 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/getArticle")
 	@ResponseBody
-	public Object getArticle(int id) {
+	public ResultData getArticle(int id) {
 		Article article = articleService.getArticle(id);
 
 		if (article == null) {
-			return id + "번 게시물이 존재하지 않습니다.";
+			return ResultData.from("F-1", Ut.f("%d번 게시물이 존재하지 않습니다.", id));
 		}
-		return article;
+		return ResultData.from("S-1", Ut.f("%d번 게시물 입니다.", id), article);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
