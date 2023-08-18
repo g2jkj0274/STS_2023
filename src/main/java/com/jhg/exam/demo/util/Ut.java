81923bd01f6d4f5ba155cdf481dd3153505c682e
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-18 12:21:13 +0900

    게시물 삭제 후 replace로 이동

--- src/main/java/com/jhg/exam/demo/util/Ut.java
+++ src/main/java/com/jhg/exam/demo/util/Ut.java
@@ -2,18 +2,51 @@ package com.jhg.exam.demo.util;
 
 public class Ut {
 	public static boolean empty(Object obj) {
-		if ( obj == null ) {
+		if (obj == null) {
 			return true;
 		}
-		if ( obj instanceof String == false ) {
+		if (obj instanceof String == false) {
 			return true;
 		}
-		String  str = (String) obj;
+		String str = (String) obj;
 
 		return str.trim().length() == 0;
 	}
-	
+
 	public static String f(String format, Object... arges) {
 		return String.format(format, arges);
+	}
+
+	public static String jsHistoryBack(String msg) {
+		return """
+				<script>
+				const msg = '%s'.trim();
+
+				if(msg.length > 0) {
+					alert(msg)
+				}
+				history.back();
+				</script>
+				""";
+	}
+
+	public static String jsReplace(String msg, String uri) {
+		if (msg == null) {
+			msg = "";
+		}
+
+		if (uri == null) {
+			uri = "";
+		}
+
+		return Ut.f("""
+				<script>
+				const msg = '%s'.trim();
+				if ( msg.length > 0 ) {
+					alert(msg);
+				}
+				location.replace('%s');
+				</script>
+				""", msg, uri);
 	}
 }
