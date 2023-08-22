52310dd867250f93ef982d636439416f041e76a8
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-22 12:00:01 +0900

    멀티게시판 개념 도입, board 테이블 생성

--- db/sb_proj_2023.sql
+++ db/sb_proj_2023.sql
@@ -25,6 +25,7 @@ CREATE TABLE `article` (
   `regDate` datetime NOT NULL,
   `updateDate` datetime NOT NULL,
   `memberId` int(10) unsigned NOT NULL,
+  `boardId` int(10) unsigned NOT NULL,
   `title` char(100) NOT NULL,
   `body` text NOT NULL,
   PRIMARY KEY (`id`)
@@ -32,10 +33,33 @@ CREATE TABLE `article` (
 
 /*Data for the table `article` */
 
-insert  into `article`(`id`,`regDate`,`updateDate`,`memberId`,`title`,`body`) values 
-(1,'2023-08-17 10:26:32','2023-08-17 10:26:32',2,'제목1','내용1'),
-(2,'2023-08-17 10:26:32','2023-08-17 10:26:32',2,'제목2','내용2'),
-(3,'2023-08-17 10:26:32','2023-08-17 10:26:32',2,'제목3','내용3');
+insert  into `article`(`id`,`regDate`,`updateDate`,`memberId`,`boardId`,`title`,`body`) values 
+(1,'2023-08-22 11:46:08','2023-08-22 11:46:08',2,0,'제목 1','내용 1'),
+(2,'2023-08-22 11:46:08','2023-08-22 11:46:08',2,0,'제목 2','내용 2'),
+(3,'2023-08-22 11:46:08','2023-08-22 11:46:08',2,0,'제목 3','내용 3');
+
+/*Table structure for table `board` */
+
+DROP TABLE IF EXISTS `board`;
+
+CREATE TABLE `board` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `regDate` datetime NOT NULL,
+  `updateDate` datetime NOT NULL,
+  `code` char(50) NOT NULL COMMENT 'notice(공지사항), free(자유게시판)...',
+  `name` char(50) NOT NULL COMMENT '게시판 이름',
+  `delStatus` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '삭제여부(0=삭제전, 1=삭제)',
+  `delDate` datetime DEFAULT NULL COMMENT '삭제날짜',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `board` */
+
+insert  into `board`(`id`,`regDate`,`updateDate`,`code`,`name`,`delStatus`,`delDate`) values 
+(1,'2023-08-22 11:46:08','2023-08-22 11:46:08','notice','공지사항',0,NULL),
+(2,'2023-08-22 11:46:08','2023-08-22 11:46:08','free','자유',0,NULL);
 
 /*Table structure for table `member` */
 
@@ -51,8 +75,8 @@ CREATE TABLE `member` (
   `name` char(20) NOT NULL,
   `nickname` char(20) NOT NULL,
   `cellphoneNo` char(20) NOT NULL,
-  `email` char(20) NOT NULL,
-  `delStatus` tinyint(1) unsigned DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
+  `email` char(50) NOT NULL,
+  `delStatus` tinyint(1) unsigned NOT NULL DEFAULT 0 COMMENT '탈퇴여부(0=탈퇴전, 1=탈퇴)',
   `delDate` datetime DEFAULT NULL COMMENT '탈퇴날짜',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -60,9 +84,9 @@ CREATE TABLE `member` (
 /*Data for the table `member` */
 
 insert  into `member`(`id`,`regDate`,`updateDate`,`loginId`,`loginPw`,`authLevel`,`name`,`nickname`,`cellphoneNo`,`email`,`delStatus`,`delDate`) values 
-(1,'2023-08-17 10:26:32','2023-08-17 10:26:32','admin','admin',7,'관리자','관리자','010-1234-1234','admin@admin.com',0,NULL),
-(2,'2023-08-17 10:26:32','2023-08-17 10:26:32','user1','user1',3,'사용자1','사용자1','010-1234-1234','user1@admin.com',0,NULL),
-(3,'2023-08-17 10:26:32','2023-08-17 10:26:32','user2','user2',3,'사용자2','사용자2','010-1234-1234','user2@admin.com',0,NULL);
+(1,'2023-08-22 11:46:08','2023-08-22 11:46:08','admin','admin',7,'관리자','관리자','01011111111','admin@gmail.com',0,NULL),
+(2,'2023-08-22 11:46:08','2023-08-22 11:46:08','user1','user1',3,'사용자1','사용자1','01011112222','user1@gmail.com',0,NULL),
+(3,'2023-08-22 11:46:08','2023-08-22 11:46:08','user2','user2',3,'사용자2','사용자2','01022222222','user2@gmail.com',0,NULL);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
