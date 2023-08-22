0753554c2aa33ba6ac0fb5e55c769e110bec42f4
g2jkj0274 <g2jkj0274@gmail.com>
2023-08-22 09:40:28 +0900

    수정 버튼 또한 본인일 때만 노출, 날짜 보여주기 형식 변경

--- src/main/java/com/jhg/exam/demo/service/ArticleService.java
+++ src/main/java/com/jhg/exam/demo/service/ArticleService.java
@@ -50,6 +50,9 @@ public class ArticleService {
 
 		ResultData actorCanDeleteRd = actorCanDelete(actorId, article);
 		article.setExtra_actorCanDelete(actorCanDeleteRd.isSuccess());
+		
+		ResultData actorCanModifyRd = actorCanModify(actorId, article);
+		article.setExtra_actorCanModify(actorCanModifyRd.isSuccess());
 	}
 
 	public void deleteArticle(int id) {
